95f7b48c91b36c9592505a288220b8fedc29eb8a
lvhua <lvhuanid@163.com>
2024-05-30 09:00:54 +0800

    feat: threejs import

---
+++ src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1 class=" text-blue-400">homePage</h1>
+        <canvas id="myCanvas" width="560" height="360" />
+    </div>
+</template>
+<script setup lang="ts">
+import {Scene} from "three"
+
+const scene = new Scene();
+
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,10 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 import type { RouteRecordRaw } from 'vue-router'
 
-const routes: RouteRecordRaw[] = [{ path: "/", component: () => import('../components/HelloWorld.vue') }];
+const routes: RouteRecordRaw[] = [
+  { path: "/", component: () => import('../pages/Home.vue') },
+  { path: "/hello", component: () => import('../components/HelloWorld.vue') }
+];
 
 const router = createRouter({
   history: createMemoryHistory(),
