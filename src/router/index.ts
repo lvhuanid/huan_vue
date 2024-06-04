b2428a1331adf28ed0abb5efb1913f7f2fdcb483
lvhua <lvhuanid@163.com>
2024-06-04 09:28:59 +0800

    chore: router url

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,7 @@ const routes: RouteRecordRaw[] = [
   },
 ];
 // createMemoryHistory()，它会完全忽略浏览器的 URL 而使用其自己内部的 URL
+// 不同的历史模式 https://router.vuejs.org/zh/guide/essentials/history-mode
 const router = createRouter({
   history: createWebHistory(),
   routes,
