a587dfa28d456711e092f1977b0fb4ef4d444dd5
lvhua <lvhuanid@163.com>
2024-06-07 11:19:28 +0800

    chore: video loop

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -11,7 +11,7 @@ import ThreeModel from "../components/ThreeModel.vue"
         <ThreeModel :width="100" :height="100" />
         <router-link to="/hello">/users/eduardo</router-link>
         <router-view></router-view>
-        <video part="video" autoplay playsinline muted src="https://videos.pexels.com/video-files/8065325/8065325-hd_1280_720_25fps.mp4" preload="metadata">
+        <video class=" w-1/4 h-1/4" part="video" autoplay playsinline muted loop src="https://videos.pexels.com/video-files/8065325/8065325-hd_1280_720_25fps.mp4" preload="metadata">
             <track kind="metadata" label="cuepoints" data-removeondestroy="">
         </video>
     </div>
