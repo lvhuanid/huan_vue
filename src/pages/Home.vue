8f0cf650cbb283434ad3f72c578fe70c1cd70f08
lvhua <lvhuanid@163.com>
2024-06-13 14:02:50 +0800

    feat: nav

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,18 +2,56 @@
 import ThreeTest from "../components/ThreeTest.vue"
 import ObjLoaderTest from "../components/ObjLoaderTest.vue"
 import ThreeModel from "../components/ThreeModel.vue"
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
 </script>
 <template>
-    <div>
+    <section>
+
+        <head>
+            <nav class="bg-white border-gray-200 dark:bg-gray-900">
+                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                        <ul
+                            class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                            <li>
+                                <a href="#"
+                                    class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">Home</a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </head>
+        <main>
+            <router-view></router-view>
+        </main>
+
         <h1 class=" text-blue-400">homePage</h1>
-        <ObjLoaderTest />
+        <!-- <ObjLoaderTest />
         <ThreeModel :width="100" :height="100" />
         <router-link to="/hello">/users/eduardo</router-link>
-        <router-view></router-view>
+        
         <video class=" w-1/4 h-1/4" part="video" autoplay playsinline muted loop src="https://videos.pexels.com/video-files/8065325/8065325-hd_1280_720_25fps.mp4" preload="metadata">
             <track kind="metadata" label="cuepoints" data-removeondestroy="">
         </video>
-        <img class=" w-1/4 h-1/4" src="/src/assets/happy.gif" alt="">
-    </div>
+        <img class=" w-1/4 h-1/4" src="/src/assets/happy.gif" alt=""> -->
+    </section>
 </template>
