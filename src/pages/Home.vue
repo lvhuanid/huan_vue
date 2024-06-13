605473ea38dc751f5714ab68455e93655a144fca
lvhua <lvhuanid@163.com>
2024-06-13 14:11:44 +0800

    chore: nav navigate

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,9 +2,7 @@
 import ThreeTest from "../components/ThreeTest.vue"
 import ObjLoaderTest from "../components/ObjLoaderTest.vue"
 import ThreeModel from "../components/ThreeModel.vue"
-import { useRoute } from 'vue-router'
 
-const route = useRoute()
 </script>
 <template>
     <section>
@@ -16,15 +14,15 @@ const route = useRoute()
                         <ul
                             class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
-                                <a href="#"
+                                <a href="/hello"
                                     class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">Home</a>
                             </li>
                             <li>
-                                <a href="#"
+                                <a href="/about"
                                     class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
                             </li>
                             <li>
-                                <a href="#"
+                                <a href="/services"
                                     class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
                             </li>
                             <li>
